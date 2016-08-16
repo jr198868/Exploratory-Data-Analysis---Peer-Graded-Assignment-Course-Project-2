ca31593b1860740a012c46c3e4614f5dc5fa8390
jr198868 <rjingraymond88@gmail.com>
2016-08-16 00:20:37 -0400

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -17,4 +17,4 @@ png("plot1.png",units="px",bg="transparent")
 
 barplot((aggTotals$Emissions)/10^6,names.arg=aggTotals$year,xlab="Year",ylab="PM2.5 Emissions (10^6 Tons)",main="Total PM2.5 Emissions From All US Sources")
 
-dev.off()
+
