500688e1e19d8a11137627bda1a8728149965554
jr198868 <rjingraymond88@gmail.com>
2016-08-17 22:52:49 -0400

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -9,7 +9,7 @@ png("plot4.png",width=500,height=500,units="px",bg="transparent")
 
 library(ggplot2)
 
-ggp <- ggplot(Combustion_NEI,aes(factor(year),Emissions/10^5)) + geom_bar(stat="identity",fill="grey",width=0.75) +
+ggplot <- ggplot(Combustion_NEI,aes(factor(year),Emissions/10^5)) + geom_bar(stat="identity",fill="grey",width=0.75) +
   theme_bw() +  guides(fill=FALSE) + labs(x="year", y=expression("Total PM"[2.5]*" Emission (10^5 Tons)")) + labs(title=expression("PM"[2.5]*" Coal Combustion Source Emissions Across US from 1999-2008"))
 
-print(ggp)
+print(ggplot)
